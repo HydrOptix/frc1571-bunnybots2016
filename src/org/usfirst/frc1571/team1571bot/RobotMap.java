d46ff9fe3c47a034ef71591ec83136335a41b781
SamuelFrazee <samuelfrazee@hotmail.com>
2016-12-02 14:49:02 -0800

    Removed autogenerated comments

--- src/org/usfirst/frc1571/team1571bot/RobotMap.java
+++ src/org/usfirst/frc1571/team1571bot/RobotMap.java
@@ -4,12 +4,6 @@ import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
 public class RobotMap {	
 	//Declare all components on the robot. RobotBuilder should have done this for me, but it didn't work. Don't try to use RobotBuilder to dynamically alter code, because the old code won't get removed.
 
