2d76f0eeb237025eb8da269a3d9fb36faea9b4c3
SamuelFrazee <samuelfrazee@hotmail.com>
2016-12-23 10:24:07 -0800

    Added extra functions for manual steering speed, limit switch values, and resetting the zero position for the steering talon.

--- src/org/usfirst/frc/team1571/robot/subsystems/DriveSystem.java
+++ src/org/usfirst/frc/team1571/robot/subsystems/DriveSystem.java
@@ -4,6 +4,7 @@ import org.usfirst.frc.team1571.robot.RobotMap;
 import org.usfirst.frc.team1571.robot.commands.*;
 
 import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
@@ -11,6 +12,7 @@ public class DriveSystem extends Subsystem {
 
     private final CANTalon steeringTalon = RobotMap.steeringTalon;
     private final CANTalon driveTalons = RobotMap.driveTalonLeftMaster;
+    private final DigitalInput limit = RobotMap.driveLimit;
 
     public void initDefaultCommand() {
     	setDefaultCommand(new DriveGamepad());
@@ -20,12 +22,29 @@ public class DriveSystem extends Subsystem {
     	double targetPosition = (position+1)/2 // convert to a 0-1 range
     							* RobotMap.steeringCountsRange	//multiply by steering range
     							+ RobotMap.steeringMinCounts;	//add the minimum of steering range
+    	steeringTalon.changeControlMode(CANTalon.TalonControlMode.Position);
     	steeringTalon.set(targetPosition);
+    }
+    
+    public void steerSpeed(double speed) {
+    	steeringTalon.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+    	steeringTalon.set(speed);
     }
     
     public void setDriveSpeed(double speed) {
     	driveTalons.set(speed * RobotMap.driveSpeed);
     }
     
+    public boolean getLimitSwitch() {
+    	return limit.get();
+    }
+    
+    public void resetPosition() {
+    	driveTalons.setPosition(0);
+    }
+    
+    public double getEncoderCounts() {
+    	return steeringTalon.get();
+    }
 }
 
