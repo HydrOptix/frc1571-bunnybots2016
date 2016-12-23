d3edf562c329e2f6f5ad5da7b3f7c4981fb6e2bd
SamuelFrazee <samuelfrazee@hotmail.com>
2016-12-23 10:29:47 -0800

    Changed shooter from  a single solenoid to a double solenoid

--- src/org/usfirst/frc/team1571/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1571/robot/subsystems/Shooter.java
@@ -3,20 +3,23 @@ package org.usfirst.frc.team1571.robot.subsystems;
 import org.usfirst.frc.team1571.robot.RobotMap;
 import org.usfirst.frc.team1571.robot.commands.*;
 
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 
 public class Shooter extends Subsystem {
 
-    private final Solenoid solenoid = RobotMap.shooterSolenoid;
+    private final DoubleSolenoid solenoid = RobotMap.shooterSolenoid;
 
     public void initDefaultCommand() {
     	setDefaultCommand(new ShootTrigger());
     }
     
     public void setExtended(boolean extended) {
-    	solenoid.set(extended);
+    	
+    	
+    	if (extended) solenoid.set(DoubleSolenoid.Value.kForward);
+    	else solenoid.set(DoubleSolenoid.Value.kReverse);
     }
 }
 
