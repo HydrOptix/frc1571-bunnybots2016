bd8a1622d8344fcbe422f3ddddd61e5cfd274a8a
SamuelFrazee <samuelfrazee@hotmail.com>
2016-12-23 10:21:46 -0800

    Added calibration routines for switch to encoder/limit switch

--- src/org/usfirst/frc/team1571/robot/commands/DriveGamepad.java
+++ src/org/usfirst/frc/team1571/robot/commands/DriveGamepad.java
@@ -3,14 +3,20 @@ package org.usfirst.frc.team1571.robot.commands;
 import org.usfirst.frc.team1571.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class DriveGamepad extends Command {
 
+	
+	private boolean calibrated;
+	
     public DriveGamepad() {
         requires(Robot.driveSystem);
+        
     }
 
     protected void initialize() {
+    	calibrated = false;
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -21,12 +27,25 @@ public class DriveGamepad extends Command {
     	double leftTrigger = Robot.oi.gamepadDriver.getRawAxis(2); 	//range from 0 to 1
     	double rightTrigger = Robot.oi.gamepadDriver.getRawAxis(3); 	//range from 0 to 1
     	
-    	
-    	if(Math.abs(leftStickX) > Robot.oi.gamepadDriver_deadzoneRadiusLStick) { //Steer only if the joystick is outside the deadzone
-    		Robot.driveSystem.steer(leftStickX);
+    	if(!calibrated) {
+    		if(Robot.driveSystem.getLimitSwitch()) {
+    			Robot.driveSystem.steerSpeed(0);
+    			Robot.driveSystem.resetPosition();
+    			calibrated = true;
+    		} else {
+    			Robot.driveSystem.steerSpeed(-.1);
+    		}
+    		
+    		
     	} else {
-    		Robot.driveSystem.steer(0);
+	    	if(Math.abs(leftStickX) > Robot.oi.gamepadDriver_deadzoneRadiusLStick) { //Steer only if the joystick is outside the deadzone
+	    		Robot.driveSystem.steer(leftStickX);
+	    	} else {
+	    		Robot.driveSystem.steer(0);
+	    	}
     	}
+    	
+    	
     	if(leftTrigger < Robot.oi.gamepadDriver_deadzoneRadiusTriggers) { //If the trigger is inside the deadzone, set it to 0
     		leftTrigger = 0;
     	}
@@ -36,6 +55,7 @@ public class DriveGamepad extends Command {
     	
     	Robot.driveSystem.setDriveSpeed(rightTrigger - leftTrigger); //Use the combined values of the triggers to drive the robot. Left is negative (reverse), and right is positive (forwards)
     	
+    	SmartDashboard.putNumber("Steering Encoder Counts", Robot.driveSystem.getEncoderCounts());
     }
 
     protected boolean isFinished() {
