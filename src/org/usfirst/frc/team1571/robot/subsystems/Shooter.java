7fd6c4f57e3584e498df90d504abb53a1c5afa47
SamuelFrazee <samuelfrazee@hotmail.com>
2017-01-01 10:22:35 -0800

    Added dart count reporting and setting

--- src/org/usfirst/frc/team1571/robot/subsystems/Shooter.java
+++ src/org/usfirst/frc/team1571/robot/subsystems/Shooter.java
@@ -5,11 +5,13 @@ import org.usfirst.frc.team1571.robot.commands.*;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Shooter extends Subsystem {
 
     private final DoubleSolenoid solenoid = RobotMap.shooterSolenoid;
+    private int dartCount = RobotMap.dartCount;
 
     public void initDefaultCommand() {
     	setDefaultCommand(new ShootTrigger());
@@ -20,6 +22,15 @@ public class Shooter extends Subsystem {
     	
     	if (extended) solenoid.set(DoubleSolenoid.Value.kForward);
     	else solenoid.set(DoubleSolenoid.Value.kReverse);
+    }
+    
+    public int getDarts() {
+    	return dartCount;
+    }
+    
+    public void setDarts(int darts) {
+    	dartCount = darts;
+    	SmartDashboard.putNumber("Darts Left", darts);
     }
 }
 
